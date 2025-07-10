ce74e4b70a0b7f989b134d9e188036d5b0a1a715
Abdullah123-Commits <abdullahmuaz002@gmail.com>
2025-07-10 12:46:28 +0500

    Initial commit: completed basic to-do list project

---
+++ script.js
@@ -0,0 +1,54 @@
+// working fine
+
+// outputList = document.getElementById('task-list');
+// console.log(outputList);
+document.addEventListener('DOMContentLoaded', function () {
+    //code
+    const form = document.querySelector('.data-input-form');
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('task-list');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();// to prevent page refresh
+
+        const taskText = taskInput.value.trim();
+
+        if (taskText !== "") {
+            const li = document.createElement('li');
+            li.textContent = taskText;
+            taskList.appendChild(li);
+
+            const doneBtn = document.createElement('button');//creating done btn
+            doneBtn.textContent = 'Done';
+            doneBtn.style.marginLeft = '20px';
+            doneBtn.style.backgroundColor = '#c8e6c9';// green
+
+            const removeBtn = document.createElement('button');//creating remove btn
+            removeBtn.textContent = 'Remove';
+            removeBtn.style.marginLeft = '20px';
+            removeBtn.style.backgroundColor = '#ffcdd2';// red
+
+            //ADDING DONE AND REMOVE BUTTON TO LIST ITEM
+            li.appendChild(doneBtn);
+            li.appendChild(removeBtn);
+            
+
+            taskInput.value = "";//clear the input for next task entry
+
+            doneBtn.addEventListener('click', () => {
+                li.style.textDecoration = "line-through";
+                });
+
+            removeBtn.addEventListener('click', () => {
+                li.remove();
+                });
+        }
+        else {
+            alert("please enter a task.");
+        }
+
+    });
+
+});
+
+
